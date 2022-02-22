1602d19c8642a492576613ce795abb75de586537
weifeng165 <467141065@qq.com>
2022-02-22 23:17:03 +0800

    test

--- test/benchmark/benchmark.cc
+++ test/benchmark/benchmark.cc
@@ -49,6 +49,7 @@ int main(int argc, char *argv[]) {
 
     unsigned long long NUM_REQUESTS(1000000);
 
+    /** 123 **/
     if (argc > 1) {
         if (0 == strcmp(argv[1], "-h") ||
             0 == strcmp(argv[1], "-?") ||
